ce59fb127c036f01ab38341684ad385371beb41c
petermal1983 <peter1983@mail.ru>
2021-05-27 23:06:15 +0500

    commit

--- src/app/note1/note1.component.ts
+++ src/app/note1/note1.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-note1',
-  templateUrl: './note1.component.html',
+  templateUrl: './note1.component.html', 
   styleUrls: ['./note1.component.css']
 })
 export class Note1Component implements OnInit {
--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -3,9 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.component.html',
-  styleUrls: ['./notes.component.css']
+  styleUrls: ['./notes.component.css'],
+  
 })
 export class NotesComponent implements OnInit {
+
+  noteNumbers: Array<any> = [1, 2, 3, 4]
+
+
+  addNote() {
+    this.noteNumbers.push(this.noteNumbers.length+1);
+    
+    console.log(this.noteNumbers);
+} 
+
 
   constructor() { }
 
